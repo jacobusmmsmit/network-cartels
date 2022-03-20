059478c1d814014f333615acd2dd8197062534be
Martin Smit <jacobusmmsmit@gmail.com>
2022-03-20 21:32:07 +0000

    Update

--- src/test.jl
+++ src/test.jl
@@ -11,19 +11,18 @@ diste(x, y) = norme(x .- y)
 disth(x, y) = acosh(1 + (2*diste(x, y)^2)/((1 - norme(x)^2)*(1 - norme(y)^2)))
 
 function construct_graph(strategies, distm)
+    sources = inverse_rle(1:5, length.(strategies))
+    destinations = collect(Iterators.flatten(strategies))
     wts = mapreduce(vcat, enumerate(strategies)) do x
         i = x[1]
-        dests = x[2]
-        map(dest -> distm[i, dest], dests)
+        i_dests = x[2]
+        map(dest -> distm[i, dest], i_dests)
     end
-    sources = inverse_rle(1:5, length.(strategies))
-    dests = collect(Iterators.flatten(strategies))
-    return SimpleWeightedDiGraph(sources, dests, wts)
+    return SimpleWeightedDiGraph(sources, destinations, wts)
 end
 
 function evaluate_cost(fn, players, strategies, shortest_paths)
     map(zip(players, strategies)) do (player, dests)
-        @show player
         edges = [(player, dest) for dest in dests]
         fn(player, edges, shortest_paths)
     end
@@ -49,7 +48,23 @@ function main(npeers = 5, α = 1.0)
 
     costs = evaluate_cost(fn, 1:5, strategies, sps)
 
-    
+    scatter(first.(peers), last.(peers))
+    wts = mapreduce(vcat, enumerate(strategies)) do x
+        i = x[1]
+        ds = x[2]
+        map(dest -> dm[i, dest], ds)
+    end
+    sources = inverse_rle(1:5, length.(strategies))
+    dests = collect(Iterators.flatten(strategies))
+    @show length.((sources, dests, wts))
+    for (s, d, w) in zip(sources, dests, wts)
+        peer_s = peers[]
+        @show s
+        @show d
+        @show w
+        plot!([s[1], d[1]], [s[2], d[2]])
+    end
+    plot
 end
 
 main()
