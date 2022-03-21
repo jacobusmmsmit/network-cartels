72ab973d55abc84474786c3ec64f03593127b8b6
jacobusmmsmit <57063134+jacobusmmsmit@users.noreply.github.com>
2022-03-21 00:35:06 +0000

    Add greedy routing

--- src/test.jl
+++ src/test.jl
@@ -8,10 +8,10 @@ using Plots
 
 norme(x) = √((x[1])^2 + (x[2])^2)
 diste(x, y) = norme(x .- y)
-disth(x, y) = acosh(1 + (2*diste(x, y)^2)/((1 - norme(x)^2)*(1 - norme(y)^2)))
+disth(x, y) = acosh(1 + (2 * diste(x, y)^2) / ((1 - norme(x)^2) * (1 - norme(y)^2)))
 
 function construct_graph(strategies, distm)
-    sources = inverse_rle(1:5, length.(strategies))
+    sources = inverse_rle(1:length(strategies), length.(strategies))
     destinations = collect(Iterators.flatten(strategies))
     wts = mapreduce(vcat, enumerate(strategies)) do x
         i = x[1]
@@ -36,35 +36,68 @@ function p2p_cost(player, strategy, sps, distm, α)
     cost
 end
 
-function main(npeers = 5, α = 1.0)
+# UnitRangeWithOut
+ur_wo(ur, without) = Iterators.flatten((ur.start:(without-1), (without+1):ur.stop))
+
+function greedy_navigate(source, destination, graph, distm)
+    cur = source
+    prev = -1
+    route = [cur]
+    while cur != destination
+        possible_next = setdiff(graph.weights[:, cur].nzind, cur)
+        if length(possible_next) == 0
+            break
+        end
+        # println(map(pn -> distm[pn, destination], possible_next))
+        # @show possible_next
+        # @show distm
+        next = possible_next[findmin(pn -> distm[pn, destination], possible_next)[2]]
+        if next == prev
+            @show next
+            @show prev
+            break
+        end
+        append!(route, next)
+        prev, cur = cur, next
+    end
+    route
+end
+
+function main(npeers=4, α=0.2)
     peers = [rand(2) for _ in 1:npeers]
+    sort!(peers, by=first)
     dm = [diste(p1, p2) for (p1, p2) in Iterators.product(peers, peers)]
     fn = (player, edges, sps) -> p2p_cost(player, edges, sps, dm, α)
 
-    strategies = [sample(1:5, rand(1:5), replace = false) for _ in 1:npeers]
+    strategies = [sample(1:npeers, rand(1:npeers), replace=false) for _ in 1:npeers]
     g = construct_graph(strategies, dm)
     ds = floyd_warshall_shortest_paths(g)
     sps = ds.dists # from i to j
 
-    costs = evaluate_cost(fn, 1:5, strategies, sps)
+    costs = evaluate_cost(fn, 1:npeers, strategies, sps)
+    social_cost = sum(costs)
 
-    scatter(first.(peers), last.(peers))
-    wts = mapreduce(vcat, enumerate(strategies)) do x
-        i = x[1]
-        ds = x[2]
-        map(dest -> dm[i, dest], ds)
+    println(greedy_navigate(1, 2, g, dm))
+
+    begin
+        p = plot(legend=false)
+        scatter!(first.(peers), last.(peers), series_annotations=text.(1:length(peers), :bottom))
+        wts = mapreduce(vcat, enumerate(strategies)) do x
+            i = x[1]
+            ds = x[2]
+            map(dest -> dm[i, dest], ds)
+        end
+        sources = inverse_rle(1:npeers, length.(strategies))
+        dests = collect(Iterators.flatten(strategies))
+        for (_s, _d, w) in zip(sources, dests, wts)
+            s = peers[_s]
+            d = peers[_d]
+            midpoint = (s[1] + d[1]) / 2, (s[2] + d[2]) / 2
+            plot!([s[1], midpoint[1]], [s[2], midpoint[2]], colour=:black, arrow=true, label="")
+            plot!([midpoint[1], d[1]], [midpoint[2], d[2]], colour=:black, label="")
+        end
+        p
     end
-    sources = inverse_rle(1:5, length.(strategies))
-    dests = collect(Iterators.flatten(strategies))
-    @show length.((sources, dests, wts))
-    for (s, d, w) in zip(sources, dests, wts)
-        peer_s = peers[]
-        @show s
-        @show d
-        @show w
-        plot!([s[1], d[1]], [s[2], d[2]])
-    end
-    plot
 end
 
-main()
+main(10)
