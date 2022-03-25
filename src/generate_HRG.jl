fdda9bf1eb7a37ea7f618e82e0bb2b5a99f5c47e
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-03-25 01:47:06 +0000

    update rng

--- src/generate_HRG.jl
+++ src/generate_HRG.jl
@@ -1,3 +1,11 @@
+using Graphs
+using SimpleWeightedGraphs
+# using GraphIO
+using StatsBase
+using Plots
+using IterTools
+using Distributions
+
 function generate_hyperbolic_graph(N::Int, α, C, T)
     if α < 0.5
         throw(ArgumentError("α must be greater than 1/2"))
@@ -6,15 +14,17 @@ function generate_hyperbolic_graph(N::Int, α, C, T)
     V = [(0.0, 0.0) for _ in 1:N]
     E = Tuple{Tuple{Float64, Float64}, Tuple{Float64, Float64}}[]
     R = 2log(N) + C
-    sources = Float64[]
-    dests = Float64[]
+    sources = Int[]
+    dests = Int[]
     weights = Float64[]
     for i in 1:N
         θ = rand(Uniform(0, 2π))
-        r = asinh((u/α)*(cosh(α*R) - 1))/α * rand()
+        # r = (acosh(1 + (cosh(α*R) - 1))/α) * rand()
+        r = asinh((rand()/α)*(cosh(α*R)-1))/α
+        # asinh((y*(cosh(a*R)-1))/a)/a
         V[i] = (r, θ)
     end
-    for i in 1:N, j in i:N
+    for i in 1:N, j in i+1:N
         dH = disth(V[i], V[j])
         Pd = 1/(1 + exp((1/(2T))*(dH-R)))
         if rand() > Pd
@@ -32,4 +42,10 @@ function generate_hyperbolic_graph(N::Int, α, C, T)
     end
     # Return graph and embedding
     return SimpleWeightedDiGraph(sources, dests, weights), (V, E)
+end
+
+g, (peers, edges) = generate_hyperbolic_graph(100, 1.0, 1.0, 1.0)
+begin
+    p = plot(legend=false, aspect_ratio=1.0, proj=:polar)
+    scatter!(first.(peers), last.(peers), series_annotations=text.(1:length(peers), :bottom))
 end
