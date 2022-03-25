ca6fbc986462cac0f373dca4e4728ab7b94e4711
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-03-25 00:48:50 +0000

    Add hyperbolic distance

--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -1,6 +1,6 @@
 norme(x) = √((x[1])^2 + (x[2])^2)
 diste(x, y) = norme(x .- y)
-disth(x, y) = acosh(1 + (2 * diste(x, y)^2) / ((1 - norme(x)^2) * (1 - norme(y)^2)))
+disth((r, θ), (r′, θ′)) = acosh(cosh(r)*cosh(r′) - sinh(r)*sinh(r′)*cos(θ - θ′))
 
 # UnitRangeWithOut
 ur_wo(ur, without) = Iterators.flatten((ur.start:(without-1), (without+1):ur.stop))
